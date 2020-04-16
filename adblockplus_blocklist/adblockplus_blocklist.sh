b34247e0c0f1a4433cf0b8c62dc698762b553534
Kevin Kok Khah Whey <khahwhey.kok@3ds.com>
2020-04-16 18:53:29 +0800

    Commit changes.

--- adblockplus_blocklist/adblockplus_blocklist.sh
+++ adblockplus_blocklist/adblockplus_blocklist.sh
@@ -16,23 +16,37 @@ if [ "$(id -u)" != "0" ] ; then
 fi
 
 echo -e " ${TICK} \e[32m Retrieving domains... \e[0m"
-curl -sS -L https://easylist-downloads.adblockplus.org/fanboy-social.txt https://raw.githubusercontent.com/k2jp/abp-japanese-filters/master/abpjf.txt https://raw.githubusercontent.com/ABPindo/indonesianadblockrules/master/subscriptions/abpindo.txt https://280blocker.net/files/280blocker_adblock.txt https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt > /root/adblock.unsorted
+curl -sS -L https://easylist-downloads.adblockplus.org/fanboy-social.txt https://raw.githubusercontent.com/k2jp/abp-japanese-filters/master/abpjf.txt https://raw.githubusercontent.com/ABPindo/indonesianadblockrules/master/subscriptions/abpindo.txt https://280blocker.net/files/280blocker_adblock.txt https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt > adblock.unsorted
 
 sleep 0.1
 
 echo -e " ${TICK} \e[32m Sorting blocklist... \e[0m"
-sort -u /root/adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > /root/adblock.sorted
+sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
 
 sleep 0.1
 
 echo -e " ${TICK} \e[32m Formatting blocklist... \e[0m"
-sed 's/[\|^]//g' < /root/adblock.sorted > /root/adblockplus_blocklist.txt
+sed 's/[\|^]//g' < adblock.sorted > adblockplus_blocklist.txt
+
+sleep 0.1
 
 echo -e " ${TICK} \e[32m Removing unnecessary files... \e[0m"
-rm /root/adblock.unsorted /root/adblock.sorted
+rm adblock.unsorted adblock.sorted
+
+sleep 0.1
 
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-mv /root/adblockplus_blocklist.txt /root/adblockplus_blocklist.txt.old && cat /root/adblockplus_blocklist.txt.old | sort | uniq >> /root/adblockplus_blocklist.txt
+mv adblockplus_blocklist.txt dblockplus_blocklist.txt.old && cat adblockplus_blocklist.txt.old | sort | uniq >> adblockplus_blocklist.txt
+
+sleep 0.1
+
+echo -e " ${TICK} \e[32m Commit changes and push to repository... \e[0m"
+cd /opt/pihole_scripts/
+git commit -a
+git pull
+git push
+
+sleep 0.1
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
 pihole -g
